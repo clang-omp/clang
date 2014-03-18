47333e9fa7f3617d6c55a25daae35ec1ab26c106
Alexey Bataev <a.bataev@hotmail.com>
2014-03-18 15:51:44 +0400

    Fixed DSA

--- lib/Sema/SemaOpenMP.cpp
+++ lib/Sema/SemaOpenMP.cpp
@@ -187,7 +187,22 @@ OpenMPClauseKind DSAStackTy::getDSA(StackTy::reverse_iterator Iter, VarDecl *D,
   E = 0;
   if (Iter == Stack.rend() - 1) {
     Kind = OMPD_unknown;
-    return OMPC_shared;
+    // OpenMP [2.9.1.1, Data-sharing Attribute Rules for Variables Referenced
+    // in a region but not in construct]
+    //  File-scope or namespace-scope variables referenced in called routines
+    //  in the region are shared unless they appear in a threadprivate
+    //  directive.
+    if (!D->isFunctionOrMethodVarDecl())
+      return OMPC_shared;
+
+    // OpenMP [2.9.1.2, Data-sharing Attribute Rules for Variables Referenced
+    // in a region but not in construct]
+    //  Variables with static storage duration that are declared in called
+    //  routines in the region are shared.
+    if (D->hasGlobalStorage())
+      return OMPC_shared;
+
+    return OMPC_unknown;
   }
   // OpenMP [2.9.1.1, Data-sharing Attribute Rules for Variables Referenced
   // in a Construct, C/C++, predetermined, p.1]
@@ -4301,7 +4316,7 @@ OMPClause *Sema::ActOnOpenMPLastPrivateClause(ArrayRef<Expr *> VarList,
     OpenMPDirectiveKind DKind;
     OpenMPDirectiveKind CurrDir = DSAStack->getCurrentDirective();
     Kind = DSAStack->getImplicitDSA(VD, DKind, PrevRef);
-    if (Kind != OMPC_shared &&
+    if ((Kind != OMPC_shared && Kind != OMPC_unknown && DKind != OMPD_unknown) &&
         (CurrDir == OMPD_for || CurrDir == OMPD_sections ||
          CurrDir == OMPD_for_simd ||
          CurrDir == OMPD_parallel_for || CurrDir == OMPD_parallel_for_simd ||
@@ -5088,7 +5103,7 @@ OMPClause *Sema::ActOnOpenMPReductionClause(ArrayRef<Expr *> VarList,
     OpenMPDirectiveKind DKind;
     OpenMPDirectiveKind CurrDir = DSAStack->getCurrentDirective();
     Kind = DSAStack->getImplicitDSA(VD, DKind, PrevRef);
-    if (Kind != OMPC_shared &&
+    if ((Kind != OMPC_shared && Kind != OMPC_unknown && DKind != OMPD_unknown) &&
         (CurrDir == OMPD_for || CurrDir == OMPD_sections ||
          CurrDir == OMPD_for_simd ||
          CurrDir == OMPD_parallel_for || CurrDir == OMPD_parallel_for_simd ||
